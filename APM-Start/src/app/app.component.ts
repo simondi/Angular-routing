1dc1dbeb38062365435402775e8148f082f49de3
ConsumerProtectionBCUser <kyle.loree@consumerprotectionbc.ca>
2019-09-27 14:00:36 -0700

    finish secondary route

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RouterEvent } from '@angular/router';
 import { AuthService } from './user/auth.service';
 import { slideInAnimation } from './app.animation';
+import { MessageService } from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -17,6 +18,10 @@ export class AppComponent {
     return this.authService.isLoggedIn;
   }
 
+  get isMessageDisplayed(): boolean {
+    return this.messageService.isDisplayed;
+  }
+
   get userName(): string {
     if (this.authService.currentUser) {
       return this.authService.currentUser.userName;
@@ -25,23 +30,38 @@ export class AppComponent {
   }
 
   constructor(private authService: AuthService,
-    private router: Router) {
+              private router: Router,
+              private messageService: MessageService
+  ) {
     router.events.subscribe((routerEvent: Event) => {
       this.checkRouterEvent(routerEvent);
     })
-}
-
-checkRouterEvent(routerEvent: Event) : void {
-  if(routerEvent instanceof NavigationStart) {
-    this.loading = true;
-``}
-
-if (routerEvent instanceof NavigationEnd ||
-    routerEvent instanceof NavigationError ||
-    routerEvent instanceof NavigationCancel ) {
-  this.loading = false;
   }
-}
+
+  checkRouterEvent(routerEvent: Event) : void {
+    if(routerEvent instanceof NavigationStart) {
+      this.loading = true;
+  ``}
+
+  if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationError ||
+      routerEvent instanceof NavigationCancel ) {
+    this.loading = false;
+    }
+  }
+
+  displayMessages(): void {
+    // Example of primary and secondary routing together
+    // this.router.navigate(['/login', {outlets: { popup: ['messages']}}]); // Does not work
+    //this.router.navigate([{outlets: { primary: ['products'], popup: ['messages']}}]); // Works
+    this.router.navigate([{ outlets: { popup: ['messages'] } }]); // Works
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this.router.navigate([{ outlets: { popup: null } }]);
+    this.messageService.isDisplayed = false;
+  }
 
   logOut(): void {
     this.authService.logout();
